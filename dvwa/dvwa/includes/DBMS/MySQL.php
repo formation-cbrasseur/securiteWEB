9d5cb69ad4ce9edccd8c2c288b858493cb058127
Jerome THEMEE <jthemee@gmail.com>
2017-02-28 15:09:43 +0100

    correct redirect lin for INSTALL
    ok

--- dvwa/dvwa/includes/DBMS/MySQL.php
+++ dvwa/dvwa/includes/DBMS/MySQL.php
@@ -82,7 +82,7 @@ dvwaMessagePush( "Data inserted into 'guestbook' table." );
 dvwaMessagePush( "<em>Setup successful</em>!" );
 if( !dvwaIsLoggedIn()){
 	dvwaMessagePush( "Please <a href='login.php'>login</a>.<script>setTimeout(function(){window.location.href='login.php'},5000);</script>" );
-	header("location:http://localhost/eni/bWAPP/install.php?install=yes");
+	header("location:http://localhost/securiteWEB/bWAPP/install.php?install=yes");
 }
 
 ?>
